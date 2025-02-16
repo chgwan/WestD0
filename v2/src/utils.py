83f4305f0aa4acdebfcffa33ed86d661ef9e2c4e
chgwan <chenguang.wan@outlook.com>
2025-02-16 19:39:59 +0800

    feat(add): infer_fn

--- v2/src/utils.py
+++ v2/src/utils.py
@@ -5,6 +5,9 @@ import numpy as np
 import pandas as pd
 import torch
 from private_modules.Torch import tools
+from private_modules.utilities import save_to_file
+import h5py
+import pickle
 
 def calc_loss_MLP(
     model,
@@ -25,3 +28,58 @@ def calc_loss_MLP(
         raise ValueError(f"Nan in loss")
     return loss
 
+def inference_fn_MLP(
+    model,
+    X: torch.tensor,
+    Y_tgt: torch.tensor,
+    Y_len,
+    batch_output_flags,
+    infos,
+    **kwargs,
+):
+    hat_data_dir = kwargs['hat_data_dir']
+    res_path = os.path.join(hat_data_dir, 'result.csv')
+    loss_fn =  tools.MaskedMSELoss(reduction='none')
+    mean = kwargs['mean']
+    stDev = kwargs['stDev']
+    mean = torch.from_numpy(mean).float().cuda()
+    stDev = torch.from_numpy(stDev).float().cuda()
+    try:
+        Y_hat = model(X)
+    except Exception as e:
+        raise Exception(str(infos[0]), repr(e))
+    batch_loss = loss_fn(Y_tgt, Y_hat, Y_len, batch_output_flags)
+    batch_loss = batch_loss.sum(dim=1).sum(dim=-1) / (Y_len * batch_output_flags.sum(dim=-1))
+    batch_loss_arr = batch_loss.detach().cpu().numpy()
+    batch_loss_mean = batch_loss.mean()
+    batch_size = batch_loss.shape[0]
+    Y_hat = Y_hat * stDev + mean
+    Y_tgt = Y_tgt * stDev + mean
+
+    Y_hat = Y_hat.cpu().numpy()
+    Y_tgt = Y_tgt.cpu().numpy()
+
+    # shot_dict = {
+    #     'Y_hat': Y_hat, 
+    #     'Y_tgt': Y_tgt,
+    #     'Y_len': Y_len.cpu().numpy(),
+    #     'batch_output_flags': batch_output_flags.cpu().numpy(),
+    #     'infos': infos,
+    # }
+    # with open('a.b', 'wb') as f:
+    #     pickle.dump(shot_dict, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+    for idx in range(batch_size):
+        info = infos[idx]
+        loss = batch_loss_arr[idx]
+        file_name = info['file_name']
+        tail_name = os.path.basename(file_name)
+        with open(res_path, 'a') as f:
+            f.write(f"{loss:.5f}, {tail_name} \n")
+
+        h5_file = os.path.join(hat_data_dir, tail_name)
+        h5_dict = dict({})
+        h5_dict['Y_hat'] = Y_hat[idx, ...]
+        h5_dict['Y_tgt'] = Y_tgt[idx, ...]
+        save_to_file(h5_file, h5_dict)
+    return batch_loss_mean
