93a86fe32dd02792c5e761744df9e114c0853c23
chgwan <chenguang.wan@outlook.com>
2025-04-04 15:33:06 +0800

    feat: add GPT

--- v2/src/mlmodels.py
+++ v2/src/mlmodels.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 import torch
 from torch import nn
 from torch import Tensor
@@ -7,6 +7,7 @@ from typing import Callable, Optional
 import math
 from private_modules.Torch import qkmodels
 from Models import Swin
+
 
 class FastLSTM(nn.Module):
     def __init__(
@@ -27,16 +28,18 @@ class FastLSTM(nn.Module):
             num_layers=num_layers,
             # proj_size=output_dim,
             dropout=dropout_rate,
-            bidirectional = bidirectional,
+            bidirectional=bidirectional,
             batch_first=True,
         )
         self.mlp = nn.Linear(
-            hidden_size * (bidirectional + 1), 
+            hidden_size * (bidirectional + 1),
             output_dim)
+
     def forward(self, X, state=None):
         Y, state = self.lstm(X, state)
         Y = self.mlp(Y)
         return Y, state
+
 
 class EmbedPostion(nn.Sequential):
     def __init__(
@@ -57,9 +60,11 @@ class EmbedPostion(nn.Sequential):
             # Make channel last to channel first
             qkmodels.Permute([0, 2, 1]),
             nn.LayerNorm(embed_dim),
-            qkmodels.PositionalEncoding(embed_dim, dropout, max_len=window_size),
+            qkmodels.PositionalEncoding(
+                embed_dim, dropout, max_len=window_size),
         )
         super().__init__(*layers)
+
 
 class WestFormer(nn.Module):
     def __init__(
@@ -145,21 +150,20 @@ class WestFormer(nn.Module):
     #     enc_l = enc_inputs.shape[1]
     #     enc_inputs = self.enc_embed(enc_inputs)
     #     src_mask = self.trans.generate_square_subsequent_mask(
-    #         enc_l, 
+    #         enc_l,
     #         device=enc_inputs.device,
     #         )
     #     enc_outputs = self.trans.encoder(enc_inputs, src_mask)
     #     # memory = enc_outputs[:, -1, :]
 
-        
     #     for _ in range(enc_l):
     #         tgt_mask = self.trans.generate_square_subsequent_mask(
     #             dec_input.size(1),
     #             device=dec_input.device)
     #         model_input = self.dec_embed(dec_input)
     #         model_output = self.trans.decoder(
-    #             model_input, 
-    #             memory=enc_outputs, 
+    #             model_input,
+    #             memory=enc_outputs,
     #             tgt_mask=tgt_mask)
     #         model_output = self.restore_layer(model_output)
     #         # print(model_output.size())
@@ -167,6 +171,7 @@ class WestFormer(nn.Module):
     #     # remove the start
     #     dec_input = dec_input[:, 1:, :]
     #     return dec_input
+
 
 class WestERT(nn.Module):
     def __init__(
@@ -192,26 +197,59 @@ class WestERT(nn.Module):
             dropout=dropout_rate,
             batch_first=True,
         )
-        self.ERT = nn.TransformerEncoder(
+        self.former = nn.TransformerEncoder(
             encoder_layer=encoder_layer,
             num_layers=num_layers)
         self.restore_layer = nn.Linear(
             in_features=embed_dim,
             out_features=output_dim,)
-    
+
     def forward(self, enc_inputs):
         enc_inputs = self.enc_embed(enc_inputs)
         maxlen = enc_inputs.size(1)
         causal_mask = nn.Transformer.generate_square_subsequent_mask(
             maxlen,
             device=enc_inputs.device,)
-        enc_outputs = self.ERT(enc_inputs, causal_mask)
+        enc_outputs = self.former(enc_inputs, causal_mask, is_causal=True)
+        Y_hat = self.restore_layer(enc_outputs)
+        return Y_hat
+
+
+class WestGPT(WestERT):
+    def __init__(self, input_dim, embed_dim, output_dim, window_size=1024, num_heads=8, num_layers=2, dropout_rate=0.1, noise_ratio=0.1, **kwargs):
+        super().__init__(input_dim, embed_dim, output_dim, window_size,
+                         num_heads, num_layers, dropout_rate, noise_ratio, **kwargs)
+        decoder_layer = nn.TransformerDecoderLayer(
+            d_model=embed_dim,
+            nhead=num_heads,
+            dropout=dropout_rate,
+            batch_first=True,
+        )
+        self.former = nn.TransformerDecoder(
+            decoder_layer=decoder_layer,
+            num_layers=num_layers,)
+
+    def forward(self, enc_inputs):
+        enc_inputs = self.enc_embed(enc_inputs)
+        maxlen = enc_inputs.size(1)
+        causal_mask = nn.Transformer.generate_square_subsequent_mask(
+            maxlen,
+            device=enc_inputs.device,)
+        
+        enc_outputs = self.former(enc_inputs, 
+                                  enc_inputs, 
+                                  causal_mask, 
+                                  causal_mask, 
+                                  tgt_is_causal=True,
+                                  memory_is_causal=True)
         Y_hat = self.restore_layer(enc_outputs)
         return Y_hat
 
 if __name__ == "__main__":
+    import os
+    os.environ['CUDA_VISIBLE_DEVICES'] = "1"
     batch_size = 1
-    seq_len = int(10 ** 4)
+    seq_len = int(10 ** 3)
     input_dim = 19
     output_dim = 6
     embed_dim = 512
@@ -219,18 +257,17 @@ if __name__ == "__main__":
     input_seq = torch.zeros(batch_size, seq_len, input_dim)
     tgt_seq = torch.zeros(batch_size, seq_len, output_dim)
 
-
     # model = WestFormer(
     #     input_dim,
     #     embed_dim,
     #     output_dim,
     #     window_size=int(5 * 10 ** 4))
-    
-    model = WestERT(input_dim, embed_dim, output_dim, window_size=int(5 * 10 ** 4))
-    
+
+    model = WestGPT(input_dim, embed_dim, output_dim,
+                    window_size=int(5 * 10 ** 3))
+
     model.cuda()
     input_seq = input_seq.cuda()
 
-    
     output_seq = model(input_seq)
     print(output_seq.shape)
